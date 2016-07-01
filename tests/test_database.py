3633691fea52f057ae7d2915cb3df47741e92e03
Laurens Versluis <laurens-versluis@hotmail.com>
2016-07-01 18:42:17 +0200

    Added tests that test the upgrade procedure of Dispersy

--- tests/test_database.py
+++ tests/test_database.py
@@ -5,6 +5,12 @@ from tempfile import mkdtemp
 
 from ..dispersydatabase import DispersyDatabase, DatabaseVersionTooLowError, DatabaseVersionTooHighError
 
+from nose.tools import raises
+from nose.twistedtools import deferred
+from twisted.internet.defer import inlineCallbacks
+
+from ..dispersydatabase import DispersyDatabase
+
 
 class TestDatabase(TestCase):
     FILE_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
@@ -13,6 +19,13 @@ class TestDatabase(TestCase):
 
     def setUp(self):
         super(TestDatabase, self).setUp()
+
+        # Do not use an in-memory database. Different connections to the same
+        # in-memory database do not point towards the same database.
+        # http://stackoverflow.com/questions/3315046/sharing-a-memory-database-between-different-threads-in-python-using-sqlite3-pa
+        if not os.path.exists(self.TEST_DATA_DIR):
+            os.mkdir(self.TEST_DATA_DIR)
+
         if not os.path.exists(self.TMP_DATA_DIR):
             os.mkdir(self.TMP_DATA_DIR)
 
@@ -22,6 +35,9 @@ class TestDatabase(TestCase):
         if os.path.exists(self.TMP_DATA_DIR):
             shutil.rmtree(self.TMP_DATA_DIR, ignore_errors=True)
 
+    @raises(RuntimeError)
+    @deferred(timeout=10)
+    @inlineCallbacks
     def test_unsupported_database_version(self):
         minimum_version_path = os.path.abspath(os.path.join(self.TEST_DATA_DIR, u"dispersy_v1.db"))
         tmp_path = os.path.join(self.TMP_DATA_DIR, u"dispersy.db")
@@ -30,19 +46,26 @@ class TestDatabase(TestCase):
         database = DispersyDatabase(tmp_path)
         self.assertRaises(DatabaseVersionTooLowError, database.open)
 
+        yield database.open()
+
+    @deferred(timeout=10)
+    @inlineCallbacks
     def test_upgrade_16_to_latest(self):
         minimum_version_path = os.path.abspath(os.path.join(self.TEST_DATA_DIR, u"dispersy_v16.db"))
         tmp_path = os.path.join(self.TMP_DATA_DIR, u"dispersy.db")
         shutil.copyfile(minimum_version_path, tmp_path)
 
         database = DispersyDatabase(tmp_path)
-        database.open()
+        yield database.open()
         self.assertEqual(database.database_version, 21)
 
+    @raises(RuntimeError)
+    @deferred(timeout=10)
+    @inlineCallbacks
     def test_upgrade_version_too_high(self):
         minimum_version_path = os.path.abspath(os.path.join(self.TEST_DATA_DIR, u"dispersy_v1337.db"))
         tmp_path = os.path.join(self.TMP_DATA_DIR, u"dispersy.db")
         shutil.copyfile(minimum_version_path, tmp_path)
 
         database = DispersyDatabase(tmp_path)
-        self.assertRaises(DatabaseVersionTooHighError, database.open)
+        yield database.open()
