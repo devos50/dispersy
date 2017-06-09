f78dac12ef43aad991017bcba6066cdc9b2e680e
Martijn de Vos <mdmartijndevos@gmail.com>
2017-06-09 16:34:17 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='dispersy',
+    packages=['dispersy'],
+    version='1.0',
+    description='The elastic database system. A database designed for P2P-like scenarios, where potentially millions of computers send database updates around.',
+    author='Tribler development team',
+    author_email='dev@tribler.org',
+    url='https://github.com/tribler/dispersy',
+    download_url='https://github.com/Tribler/dispersy/archive/v1.0.tar.gz',
+    keywords=['python', 'peer-to-peer', 'decentralized', 'messaging', 'database'],
+    classifiers=[],
+)
